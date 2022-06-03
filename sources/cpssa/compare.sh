68e21778a5ce74cf400e174e9d2f720ccad8719d
Tony Bowden <tony@tmtm.com>
2022-06-03 20:39:07 +0300

    Turn off CPSSA recurring comparisons

--- sources/cpssa/compare.sh
+++ sources/cpssa/compare.sh
@@ -5,9 +5,8 @@
 
 cd $(dirname $0)
 
-qsv join --left position cpssa.csv raw reconciled-positions.csv | qsv select minister,position\[1\],raw,startdate,enddate | qsv join --left minister - name reconciled-people.csv | qsv select id,minister,position,raw,startdate,enddate | qsv rename item,name,positionItem,position,start,end | sed -e 's/2019-12-31//' > scraped.csv
-
-wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv search -s start . > wikidata.csv
-bundle exec ruby diff.rb | qsv sort -s name,position | tee diff.csv
+# qsv join --left position cpssa.csv raw reconciled-positions.csv | qsv select minister,position\[1\],raw,startdate,enddate | qsv join --left minister - name reconciled-people.csv | qsv select id,minister,position,raw,startdate,enddate | qsv rename item,name,positionItem,position,start,end | sed -e 's/2019-12-31//' > scraped.csv
+# wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv search -s start . > wikidata.csv
+# bundle exec ruby diff.rb | qsv sort -s name,position | tee diff.csv
 
 cd ~-
