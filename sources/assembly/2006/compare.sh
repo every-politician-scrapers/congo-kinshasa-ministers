4a8901506a6d12effec80e59dc2fe6f4f3436575
Tony Bowden <tony@tmtm.com>
2022-06-07 15:50:40 +0300

    initial 2006 Assembly data

---
+++ sources/assembly/2006/compare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+# Static file generated from source
+
+wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv sort -s itemLabel,startDate > wikidata.csv
+
+bundle exec ruby diff.rb | qsv sort -s itemlabel | tee diff.csv
+
+cd ~-
